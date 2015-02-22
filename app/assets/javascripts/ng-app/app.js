627731f1c4ada9aa2b6ffd9b9e27667306d1cfac
Onaracs <brendan.scarano@gmail.com>
2015-02-22 16:28:00 -0500

    set up routing

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -6,4 +6,15 @@ angular
     ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
   });
